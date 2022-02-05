fa5ef5fdbd13c326792f8dab4f1efb940f428988
annava28 <97325167+annava28@users.noreply.github.com>
2022-02-05 00:14:23 -0500

    final -- validation corrected through input standardization

--- game.py
+++ game.py
@@ -11,19 +11,23 @@ print("---------------------------------------------------------------")
 # ask for user input
 user_weapon = input("Please select your weapon: 'Rock', 'Paper', 'Scissors': ")
 print("---------------------------------------------------------------")
+
 # validations
+#https://www.w3schools.com/python/ref_string_capitalize.asp
+
 acceptable = "Rock rock ROCK paper Paper PAPER Scissors scissors SCISSORS"
 
 if user_weapon not in acceptable:
     print ("Oh no! Please ensure you have selected correctly from the above weapons.")
     quit()
 else: 
+    user_weapon = user_weapon.capitalize() #standardization of inputs for validation
     print(player_name, "chose:", user_weapon)
     
 # computer choice
 
 import random
-options = ["rock", "paper", "scissors"]
+options = ["Rock", "Paper", "Scissors"]
 
 ai_weapon = random.choice(options)
 
@@ -34,22 +38,26 @@ print("---------------------------------------------------------------")
 # adapted from Marie-Eugenie Chandon-Moet on Slack
 
 if user_weapon == ai_weapon:
-    print("Both players played", user_weapon, "An even match!")
-elif user_weapon == "paper":
-    if ai_weapon == "rock":
+    print("Both players played", user_weapon, ". It's a tie!")
+elif user_weapon == "Paper":
+    if ai_weapon == "Rock":
         print("Paper covers rock. You won!")
     else:
         print("Scissors cuts paper. You lost. Better luck next time!")
-elif user_weapon == "scissors":
-    if ai_weapon == "paper":
+elif user_weapon == "Scissors":
+    if ai_weapon == "Paper":
         print("Scissors cuts paper. You won!")
     else:
         print("Rock crushes scissors. You lost. Better luck next time!")
-elif user_weapon == "rock":
-    if ai_weapon == "scissors":
+elif user_weapon == "Rock":
+    if ai_weapon == "Scissors":
         print("Rock crushes scissors. You won!")
     else:
         print("Paper covers rock. You lost. Better luck next time!")
+else:
+    print("Computer error. Please try re-running the program!")
+
+
 
 # goodbye message
 print("---------------------------------------------------------------")
